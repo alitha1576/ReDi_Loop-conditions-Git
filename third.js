e36e7c75d9f1bb80d068426b54c77e80c5a95b97
Anna Petrova <ann.undine@gmail.com>
2024-10-24 11:19:22 +0200

    create a new branch

--- third.js
+++
@@ -1,5 +0,0 @@
-// 3) Print out every third number between 1 and 100. Try to limit the number of iterations (it should be less than 100). 
-
-for (let i = 1; i < 100; i +=3) {
-    console.log(i)
-}
